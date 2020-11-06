51c42d1307a9e8c351ce6ea620947a63c70283df
Alexis Mora <alexis.mora.sanchez@gmail.com>
2020-11-06 12:10:32 +0100

    fixed bug that did not allowed to send products to the view

--- routes/views/products.js
+++ routes/views/products.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const ProductsService = require("../../services/products")
+
 const productService= new ProductsService()
 
 router.get('/', async (request, response,next) => {
     const {tags} = request.query
     try {
-        const products = await productsService.getProducts({tags})
+        const products = await productService.getProducts({tags})
         response.render('products', { products })    
     } catch (error) {
         next(error)        
